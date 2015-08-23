124fe73502f56446fa79b1f905f132c440ab7eb6
Andreas Horstmann <datmass92@gmail.com>
2015-08-23 14:08:25 +0200

    Ein bischen aufgeraemt

---
+++ Project_Alpha/Classes/Manager/InputManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project_Alpha
+{
+    class InputManager
+    {
+        private KeyboardState ks = Keyboard.GetState();
+        private KeyboardState oldKS;
+        private MouseState ms;
+        private static InputManager instance;
+        public InputState state;
+        public static InputManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new InputManager();
+                return instance;
+            }
+        }
+
+
+        private InputManager()
+        {
+
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            if (state == InputState.Menu)
+            {
+
+            }
+            else if (state == InputState.Pause)
+            {
+
+            }
+            else if(state == InputState.Playing)
+            {
+
+            }
+            else if (state == InputState.Text)
+            {
+
+            }
+            oldKS = ks;
+        }
+    }
+
+    /// <summary>
+    /// Status zum unterscheiden von Eingabesituationen
+    /// </summary>
+    public enum InputState
+    {
+        Text,
+        Playing,
+        Menu,
+        Pause
+    }
+}
