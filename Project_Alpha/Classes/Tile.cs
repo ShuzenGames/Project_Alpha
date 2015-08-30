423cd011b215d5d623b2cf6c342f71f7266b244b
Sascha Zander <sascha.zander@denkwerk.com>
2015-08-30 17:53:54 +0200

    nzufügen einer Tilemap

---
+++ Project_Alpha/Classes/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project_Alpha.Classes
+{
+    class Tile
+    {
+        public Vector2 pos;
+        private Texture2D texture;        
+        private Movable movable; //gibt an ob der Tile passierbar ist. Gibt ggf. die richtung an
+        private bool isOverlaying; //noch nicht eingebunden
+        private Rectangle rect; //Größe des Tilefeldes
+
+        public Tile(Vector2 pos, Texture2D texture, Movable movable, Rectangle rect)
+        {
+            this.pos = pos;
+            this.texture = texture;
+            this.movable = movable;
+            this.rect = rect;
+        }
+
+        public void Update(GameTime gt)
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(this.texture, this.pos, Color.White);
+        }
+    }
+
+    public enum Movable
+    {
+        All_sides,
+        Vertical,
+        Horizontal,
+        Underneath,
+        Not
+    }
+}
---
+++ Project_Alpha/Classes/TileMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_Alpha.Classes
+{
+    class TileMap
+    {
+        string name;
+        int width, height; // angabe in tiles
+        Tile tiles;
+
+        public TileMap(string name, int width, int height)
+        {
+
+        }
+    }
+}
